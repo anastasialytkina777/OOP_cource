762e923d2b86ebdff134ea4fa280c0381fc15db9
Anastasia <anastasialytkina027@gmail.com>
2025-03-07 14:19:40 +0300

    first commit

---
+++ OOP/src/Lab1/Task1/task1.java
@@ -0,0 +1,15 @@
+package Lab1.Task1;
+
+import java.util.Scanner;
+
+public class task1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter youe age:  ");
+        int age = scanner.nextInt();
+        System.out.println(age);
+        scanner.close();
+
+
+}
+}
