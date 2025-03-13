458f05585c7973939b8a6a8ba591e8a6c026818b
anastasialytkina777 <anastasialytkina027@gmail.com>
2025-03-14 01:17:54 +0300

    first commit

--- OOP/src/Lab1/task3/Task3.java
+++ OOP/src/Lab1/task3/Task3.java
@@ -1,10 +1,4 @@
 package Lab1.task3;
 
-public class Task3 {public static void main(String[] args) {
-    byte num1;
-    num1 = 6;
-    double num2 = num1 * 6.6;
-    System.out.println(num2);
-}
-
+public class Task3 {
 }
