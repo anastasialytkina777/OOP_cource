89b2669873a958a4cde6f5a422751cb4d490cd58
anastasialytkina777 <anastasialytkina027@gmail.com>
2025-03-21 01:36:33 +0300

    second commit

---
+++ OOP/src/Lab2/task7/Task7.java
@@ -0,0 +1,17 @@
+package Lab2.task7;
+import java.util.Scanner;
+import java.util.Random;
+public class Task7 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+        System.out.print("Введіть мінімальне значення: ");
+        int min = scanner.nextInt();
+        System.out.print("Введіть максимальне значення: ");
+        int max = scanner.nextInt();
+        int randomInt = random.nextInt(max - min + 1) + min;
+        String evenOdd = randomInt % 2 == 0 ? "парним" : "непарним";
+        System.out.printf("Згенероване число: %d%nЧисло %d є %s%n", randomInt, randomInt, evenOdd);
+        scanner.close();
+    }
+}
---
+++ OOP/src/Lab2/task8/Task8.java
@@ -0,0 +1,16 @@
+package Lab2.task8;
+import java.util.Scanner;
+public class Task8 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введіть електронну пошту: ");
+
+        String email = scanner.nextLine();
+        int atIndex = email.indexOf("@");
+        String username = email.substring(0, atIndex);
+        String domain = email.substring(atIndex + 1);
+        System.out.println("Username: " + username);
+        System.out.println("Domain: " + domain);
+        scanner.close();
+    }
+}
